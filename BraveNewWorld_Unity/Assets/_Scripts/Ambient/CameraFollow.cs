750345340d21289c556e8481f416e4bacf3335ac
remi_salomon <remisalomon.pro@gmail.com>
2018-03-21 14:57:08 +0100

    Màj nettoyage

--- BraveNewWorld_Unity/Assets/_Scripts/Ambient/CameraFollow.cs
+++ BraveNewWorld_Unity/Assets/_Scripts/Ambient/CameraFollow.cs
@@ -12,12 +12,14 @@ public class CameraFollow : MonoBehaviour {
     {
 		if (GameObject.FindGameObjectWithTag ("Player"))
         {
-			Vector3 targetDir = target.position - transform.position;
-			float step = speed * Time.deltaTime;
-			Vector3 newDir = Vector3.RotateTowards (transform.forward, targetDir, step, 0.0F);
-			Debug.DrawRay (transform.position, newDir, Color.red);
-			//newDir.y = 0f;
-			transform.rotation = Quaternion.LookRotation (newDir);
-		}
+            //Vector3 targetDir = target.position - transform.position;
+            //float step = speed * Time.deltaTime;
+            //Vector3 newDir = Vector3.RotateTowards (transform.forward, targetDir, step, 0.0F);
+            //Debug.DrawRay (transform.position, newDir, Color.red);
+            //newDir.y = 0f;
+            //transform.rotation = Quaternion.LookRotation (newDir);
+
+            transform.LookAt(target);
+        }
 	}
 }
