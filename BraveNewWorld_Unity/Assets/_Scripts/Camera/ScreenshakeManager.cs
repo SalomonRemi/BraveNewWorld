c0e896397a6f601d91d5137b57ebf478f9f4970b
remi_salomon <remisalomon.pro@gmail.com>
2018-03-29 12:26:59 +0200

    Screenshake en cours

---
+++ BraveNewWorld_Unity/Assets/_Scripts/Camera/ScreenshakeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshakeManager : MonoBehaviour {
+
+    public CameraShake.Properties elevatorLongShake;
+    public CameraShake.Properties elevatorEndShake;
+
+    private bool isInElevator = true;
+
+
+    private void Start()
+    {
+        StartCoroutine(TimingCoroutine());
+    }
+
+
+    void Update ()
+    {
+        if (isInElevator) FindObjectOfType<CameraShake>().StartShake(elevatorLongShake);
+        else FindObjectOfType<CameraShake>().StartShake(elevatorEndShake);
+	}
+
+
+    IEnumerator TimingCoroutine()
+    {
+        isInElevator = true;
+
+        yield return new WaitForSeconds(MissionManager.instance.timeInElevator);
+
+        isInElevator = false;
+    }
+}
