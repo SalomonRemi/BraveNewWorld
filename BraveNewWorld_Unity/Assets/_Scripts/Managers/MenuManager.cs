405d28dfc33bc75da4bd6216a206233b0065e18a
remi_salomon <remisalomon.pro@gmail.com>
2018-04-05 12:27:27 +0200

    Ajout loading screen en cours

--- BraveNewWorld_Unity/Assets/_Scripts/Managers/MenuManager.cs
+++ BraveNewWorld_Unity/Assets/_Scripts/Managers/MenuManager.cs
@@ -2,16 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour {
 
+    public GameObject loadingScreen;
+    public GameObject uiMenu;
+    public Slider slider;
+
 	public void LoadLevel(string name)
     {
-        SceneManager.LoadScene(name);
+        StartCoroutine(LoadSceneAsynchro(name));
+
+        uiMenu.SetActive(false);
+        loadingScreen.SetActive(true);
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    IEnumerator LoadSceneAsynchro(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress);
+
+            slider.value = progress;
+
+            yield return null;
+        }
     }
 }
